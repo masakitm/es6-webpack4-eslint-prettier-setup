2c58a4c674ede863efd253114e0096daee5586bd
masaki.tomotsuka <masaki.tomotsuka@asia-quest.jp>
2018-04-25 10:24:08 +0900

    add loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const MODE = 'development';
+const enabledSourceMap = (MODE === 'development');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -8,5 +11,33 @@ module.exports = {
   devServer: {
     contentBase: 'dist',
     open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2,
+            }
+          }
+        ]
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: enabledSourceMap
+        }
+      },
+      {
+        test: /\.(gif|png|jpg|eot||wof|woff|woff2|ttf|svg)$/,
+        loader: 'url-loader'
+      }
+    ]
   }
 };
